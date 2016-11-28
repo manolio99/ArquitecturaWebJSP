05e2041d18fa843439a140809ad68b2497919976
mplanell <mplanells84@gmail.com>
2016-11-28 22:10:14 +0100

    MPJ - prueba de commit (espacios en blanco)

--- src/main/java/com/arquitecturajava/aplicacion/JDBCHelper.java
+++ src/main/java/com/arquitecturajava/aplicacion/JDBCHelper.java
@@ -15,7 +15,7 @@ public class JDBCHelper<T> {
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://localhost/arquitecturajava";
 	private static final String USUARIO = "root";
-	private static final String CLAVE = "root";
+	private static final String CLAVE = "root";      
 
 	private static final Logger log = Logger.getLogger(JDBCHelper.class.getPackage().getName());
 
